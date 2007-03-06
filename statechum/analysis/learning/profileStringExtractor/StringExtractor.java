0441af2497e605ca1d7cd366e386d1252ada31d1
Neil Walkinshaw <nw91@le.ac.uk>
2007-03-06 15:29:47 +0000

    Changed to integrate with the Blue Fringe learner
    git-svn-id: svn://svn.code.sf.net/p/statechum/code/XMachineTool/trunk@4 4b8d0e13-f52a-0410-bc0e-8c3b9955e25a

--- statechum/analysis/learning/profileStringExtractor/StringExtractor.java
+++ statechum/analysis/learning/profileStringExtractor/StringExtractor.java
@@ -1,22 +1,9 @@
 package statechum.analysis.learning.profileStringExtractor;
 
-import java.io.*;
-import javax.swing.*;
-
 public class StringExtractor {
 
 	public static void main(String[] args){
-		JFileChooser fc = new JFileChooser();
-		fc.setMultiSelectionEnabled(true);
-		fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
-		JFrame frame = new JFrame();
-		frame.setVisible(true);
-		int choice = fc.showDialog(frame, "Select XML files");
-		if(choice == JFileChooser.APPROVE_OPTION){
-			File[] file = fc.getSelectedFiles();
-			Extractor e = new Extractor(file);
-		}
-		frame.dispose();
+		SplitFrame sFrame = new SplitFrame();
 	}
 	
 }

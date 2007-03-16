531b39e61b08ac8a634be4278545b1d0f1262c9c
Neil Walkinshaw <nw91@le.ac.uk>
2007-03-16 17:46:02 +0000

    failed effort to create new branch
    git-svn-id: svn://svn.code.sf.net/p/statechum/code/XMachineTool/trunk@49 4b8d0e13-f52a-0410-bc0e-8c3b9955e25a

--- newQuestioning/statechum/JUConstants.java
+++
@@ -1,9 +0,0 @@
-package statechum;
-
-public interface JUConstants {
-	final String LABEL = "label";
-	final String ACCEPTED = "accepted";
-	final String TITLE = "title";
-	final String TRUE = "true";
-	final String FALSE = "false";
-}

d5d5268a983729088f5d11c7d41474f76b6896a2
Kirill Bogdanov <k.bogdanov@sheffield.ac.uk>
2007-04-17 00:58:42 +0000

    added support for executing a job when the main joblot finishes. The job executed last is finished.sh which by default dumps the current date.
    git-svn-id: svn://svn.code.sf.net/p/statechum/code/XMachineTool/trunk@109 4b8d0e13-f52a-0410-bc0e-8c3b9955e25a

--- NightTrain/submit.sh
+++ NightTrain/submit.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 MACHINES=machines
+/bin/date > output/started
 MACHNO=`wc -l $MACHINES|awk '{ print $1; }'`
-qsub -cwd -l qp=LOW -l mclass=c1 -t 1-$MACHNO -j y -o output/LOG learner.sh $MACHINES
+qsub -cwd -l qp=LOW -l mclass=c1 -t 1-$MACHNO -j y -o output/LOG learner.sh $MACHINES 
+#| awk '{ gsub("\\..*","",$3);print $3; }'`
+qsub -cwd -l qp=LOW -l mclass=c3 -j y -o output/finished -hold_jid learner.sh finished.sh
 

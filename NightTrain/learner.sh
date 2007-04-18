39a874688354f1aa3d22e39db7d542ba0ea726cb
Kirill Bogdanov <k.bogdanov@sheffield.ac.uk>
2007-04-18 00:14:48 +0000

    minor improvements to support per-percent parallelism
    git-svn-id: svn://svn.code.sf.net/p/statechum/code/XMachineTool/trunk@112 4b8d0e13-f52a-0410-bc0e-8c3b9955e25a

--- NightTrain/learner.sh
+++ NightTrain/learner.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-FILE=`awk 'BEGIN { k = 1; } { if (k == ENVIRON["SGE_TASK_ID"]) { print $1 } ++k; }' $1`
+PERCENTSTAGES=10
+TASK_ID=`expr $SGE_TASK_ID - 1`
+FILENO=`expr $TASK_ID '/' $PERCENTSTAGES`
+echo number : $FILENO \$ $$
+FILE=`awk "BEGIN { k = 0; } { if (k == $FILENO ) { print ; } ++k; }" $1`
 STATECHUM=..
-java -cp $STATECHUM/lib/commons-collections-3.1.jar:$STATECHUM/lib/colt.jar:$STATECHUM/lib/jung-1.7.4.jar:$STATECHUM/NightTrain/lib/junit-4.1.jar:$STATECHUM/bin -ea -Xmx600m -XX:NewRatio=1 statechum.analysis.learning.AccuracyAndQuestionsExperiment $FILE junk >output/$SGE_TASK_ID.txt 2>&1
+java -cp $STATECHUM/lib/commons-collections-3.1.jar:$STATECHUM/lib/colt.jar:$STATECHUM/lib/jung-1.7.4.jar:$STATECHUM/NightTrain/lib/junit-4.1.jar:$STATECHUM/bin -ea -Xmx600m -XX:NewRatio=1 statechum.analysis.learning.AccuracyAndQuestionsExperiment $FILE `expr $TASK_ID '%' $PERCENTSTAGES` 
 
 

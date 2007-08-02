f1ae838a8fa035282ea296bd17253a32d34d88fc
Neil Walkinshaw <nw91@le.ac.uk>
2007-08-02 09:40:00 +0000

    refactored
    git-svn-id: svn://svn.code.sf.net/p/statechum/code/XMachineTool/trunk@143 4b8d0e13-f52a-0410-bc0e-8c3b9955e25a

--- statechum/analysis/learning/profileStringExtractor/AbstractFunctionFrame.java
+++ statechum/analysis/learning/profileStringExtractor/AbstractFunctionFrame.java
@@ -187,34 +187,7 @@ public class AbstractFunctionFrame extends JFrame implements ActionListener{
 				for(int i=0;i<files.length;i++){
 					SAXParser parser = factory.newSAXParser();
 					
-					StackHandler stackHandler = new 
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					StackHandler(namesToMethods, filesToHandlers.get(files[i]));
+					SequenceHandler stackHandler = new SequenceHandler(namesToMethods, filesToHandlers.get(files[i]));
 					parser.parse((File)files[i], stackHandler);
 					System.out.println(stackHandler.getFunctionString(3));
 					sPlus.add(stackHandler.getArrayListFunctionString(3));

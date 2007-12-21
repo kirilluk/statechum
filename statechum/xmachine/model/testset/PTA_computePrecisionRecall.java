88826a20febd1109acce3c3014c85fdfa14a69d2
Kirill Bogdanov <k.bogdanov@sheffield.ac.uk>
2007-12-21 15:00:56 +0000

    added support for precision and recall calculations by counting the number of transitions followed (i.e. by consideration of all prefixes)
    git-svn-id: svn://svn.code.sf.net/p/statechum/code/XMachineTool/trunk@171 4b8d0e13-f52a-0410-bc0e-8c3b9955e25a

---
+++ statechum/xmachine/model/testset/PTA_computePrecisionRecall.java
@@ -0,0 +1,109 @@
+package statechum.xmachine.model.testset;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Map.Entry;
+
+import statechum.analysis.learning.TestFSMAlgo.FSMStructure;
+import statechum.analysis.learning.experiments.PosNegPrecisionRecall;
+
+/** Computes precision and recall by tracing a given pta structure through this machine. 
+ * In order to exclude specific paths, it is possible to multiply this one by their pta. 
+ */
+public class PTA_computePrecisionRecall extends PTA_FSMStructure {
+
+	public PTA_computePrecisionRecall(FSMStructure machine) {
+		super(machine);
+	}
+
+	int pos_Ret, pos_Rel, pos_relret, neg_Ret, neg_Rel, neg_relret;
+	
+	protected static class PosNegPrecisionRecallNum extends PosNegPrecisionRecall
+	{
+// Very strange: if we do not expect any negative (or any positive) sequences, the final precision is always zero.
+		public PosNegPrecisionRecallNum(int pos_Ret, int pos_Rel, int pos_relret, int neg_Ret, int neg_Rel, int neg_relret) {
+			super(new HashSet(),new HashSet(), new HashSet(), new HashSet());
+			
+			negprecision = neg_Ret>0?(double)neg_relret/(double)neg_Ret:0;
+			posprecision = pos_Ret>0?(double)pos_relret/(double)pos_Ret:0;
+			negrecall = neg_Rel>0?(double)neg_relret/(double)neg_Rel:0;
+			posrecall = pos_Rel>0?(double)pos_relret/(double)pos_Rel:0;
+			precision = (2*negprecision*posprecision)/(negprecision+posprecision);
+			recall = (2*negrecall*posrecall)/(negrecall+posrecall);
+			fMeasure = (2*precision*recall)/(precision+recall);			
+		}
+	}
+
+	private void updateCounters(Node currentExplorationNode, Node currentLearntNode,PTATestSequenceEngine pc)
+	{
+		//System.out.println("matching "+currentExplorationNode+" and "+currentLearntNode);
+		/* First, we process a pair of the two nodes being considered. */ 
+		if (currentExplorationNode == pc.rejectNode && currentLearntNode == rejectNode)
+		{
+			neg_relret++;neg_Rel++;neg_Ret++;
+		}
+		if (currentExplorationNode == pc.rejectNode && currentLearntNode != rejectNode)
+		{
+			neg_Rel++;pos_Ret++;
+		}
+		if (currentExplorationNode != pc.rejectNode && currentLearntNode == rejectNode)
+		{
+			pos_Rel++;neg_Ret++;
+		}
+		if (currentExplorationNode != pc.rejectNode && currentLearntNode != rejectNode)
+		{
+			pos_relret++;pos_Rel++;pos_Ret++;
+		}		
+	}
+	
+	/** Calculates precision/recall when supplied with a specific set of sequences (such as a test set, backed by an FSM).
+	 * 
+	 * When we record sequences, non-existing transitions at the ends are recorded too, however if we have a long
+	 * non-existing path, only the first non-existing transition is being recorded. This is in compliance with
+	 * the training data which only has one non-existing transition at the end of every non-existing path
+	 * (by construction, we do not build long paths with multiple non-existing transitions).
+	 * 
+	 * @param pc the set of sequences.
+	 * @return precision/recall values 
+	 */
+	public PosNegPrecisionRecall crossWith(PTATestSequenceEngine pc)
+	{
+		pos_Ret = 0;pos_Rel = 0;pos_relret = 0;neg_Ret = 0;neg_Rel = 0;neg_relret = 0;
+		
+		Queue<Node> currentExplorationBoundary = new LinkedList<Node>(), currentLearntBoundary = new LinkedList<Node>();// FIFO queue
+		currentExplorationBoundary.add(pc.init);currentLearntBoundary.add(init);
+		
+		while(!currentExplorationBoundary.isEmpty()) // we explore all of the pc supplied
+		{
+			Node currentExplorationNode = currentExplorationBoundary.remove(), currentLearntNode = currentLearntBoundary.remove();
+			Map<String,Node> explorationRow = pc.pta.get(currentExplorationNode), learntRow = pta.get(currentLearntNode);
+
+			if (!explorationRow.isEmpty())
+				for(Entry<String,Node> entry:explorationRow.entrySet())
+				{// for each outgoing transition of a test, we need to do something with the current transition of a machine
+					Node nextLearntNode=learntRow.get(entry.getKey());
+					if (nextLearntNode == null)
+					{// if we've not seen this entry already, proceed (below). Otherwise, ignore.
+					 // Note that we also end up here if the current node is a reject node.
+
+						if (currentLearntNode != rejectNode) 
+							// Unlike methods cross..., we do not really have to update a tree here - it is enough to simply 
+							// track the current state in the learnt automata. However, updating makes it possible to implement
+							// precision/recall analysis where I first identify states and subsequently perform analysis
+							// of their behaviour compared to the original automaton.
+							nextLearntNode = followToNextNode(currentLearntNode,entry.getKey());
+						else
+							nextLearntNode=rejectNode;
+
+						updateCounters(entry.getValue(),nextLearntNode,pc);
+					}
+					
+					currentExplorationBoundary.offer(entry.getValue());currentLearntBoundary.offer(nextLearntNode);
+				}
+		}
+		
+		return new PosNegPrecisionRecallNum(pos_Ret, pos_Rel, pos_relret, neg_Ret, neg_Rel, neg_relret);
+	}
+}
---
+++ statechum/xmachine/model/testset/TestPTA_computePrecisionRecall.java
@@ -0,0 +1,204 @@
+package statechum.xmachine.model.testset;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import junit.framework.JUnit4TestAdapter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.uci.ics.jung.graph.impl.DirectedSparseGraph;
+import edu.uci.ics.jung.graph.impl.DirectedSparseVertex;
+import edu.uci.ics.jung.utils.UserData;
+
+import statechum.JUConstants;
+import statechum.analysis.learning.TestFSMAlgo;
+import statechum.analysis.learning.TestFSMAlgo.FSMStructure;
+import statechum.analysis.learning.experiments.PosNegPrecisionRecall;
+import statechum.xmachine.model.testset.PTATestSequenceEngine.Node;
+import statechum.xmachine.model.testset.PTATestSequenceEngine.sequenceSet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TestPTA_computePrecisionRecall {
+
+	private PTATestSequenceEngine en = null; 
+	private FSMStructure fsm = null;
+	
+	@Before
+	public final void setUp()
+	{
+		fsm = WMethod.getGraphData(TestFSMAlgo.buildGraph("A-a->B-a->A-b-#C\nB-b->D-c->E\nD-a-#F", "test automaton"));
+		en = new PTA_FSMStructure(fsm);		
+	}
+	
+	@Test
+	public final void test_sequenceSet1()
+	{
+		sequenceSet seq = en.new sequenceSet();
+		seq.crossWithSet(Arrays.asList(new String[] {"a"})); // appending anything to an empty sequence produces an empty sequence.
+		TestPTATestSequenceEngine.vertifyPTA(en, new String[][] {
+				new String[] {}
+		});
+	}
+	
+	@Test
+	public final void test_1()
+	{
+		FSMStructure mach = WMethod.getGraphData(TestFSMAlgo.buildGraph("A-a->A-b-#B","tmp graph"));
+		PTATestSequenceEngine engine = new PTA_FSMStructure(mach);
+		sequenceSet partialPTA = engine.new sequenceSet();partialPTA.setIdentity();
+		partialPTA = partialPTA.cross(TestFSMAlgo.buildSet(new String[][] {
+				new String[] {"a","a","a"}, new String[]{"b"}
+			}));
+		PTA_computePrecisionRecall precComputer = new PTA_computePrecisionRecall(fsm);
+		precComputer.crossWith(engine);
+		assertEquals(3, precComputer.pos_relret);assertEquals(1, precComputer.neg_relret);
+		assertEquals(3, precComputer.pos_Rel);assertEquals(1, precComputer.neg_Rel);
+		assertEquals(3, precComputer.pos_Ret);assertEquals(1, precComputer.neg_Ret);
+	}
+
+	@Test
+	public final void test_2()
+	{
+		FSMStructure mach = WMethod.getGraphData(TestFSMAlgo.buildGraph("A-a->A-b-#B","tmp graph"));
+		PTATestSequenceEngine engine = new PTA_FSMStructure(mach);
+		sequenceSet partialPTA = engine.new sequenceSet();partialPTA.setIdentity();
+		partialPTA = partialPTA.cross(TestFSMAlgo.buildSet(new String[][] {
+				new String[] {"a","a","a"}, new String[]{"b"}, new String[]{"a", "b"}
+			}));
+		PTA_computePrecisionRecall precComputer = new PTA_computePrecisionRecall(fsm);
+		precComputer.crossWith(engine);
+		assertEquals(3, precComputer.pos_relret);assertEquals(1, precComputer.neg_relret);
+		assertEquals(3, precComputer.pos_Rel);assertEquals(2, precComputer.neg_Rel);
+		assertEquals(4, precComputer.pos_Ret);assertEquals(1, precComputer.neg_Ret);
+	}
+
+	@Test
+	public final void test_3()
+	{
+		FSMStructure mach = WMethod.getGraphData(TestFSMAlgo.buildGraph("A-a->A-b-#B","tmp graph"));
+		PTATestSequenceEngine engine = new PTA_FSMStructure(mach);
+		sequenceSet partialPTA = engine.new sequenceSet();partialPTA.setIdentity();
+		partialPTA = partialPTA.cross(TestFSMAlgo.buildSet(new String[][] {
+				new String[] {"a","a","a","a","b"}, new String[]{"b"}, new String[]{"a", "b"}
+			}));
+		PTA_computePrecisionRecall precComputer = new PTA_computePrecisionRecall(fsm);
+		precComputer.crossWith(engine);
+		assertEquals(4, precComputer.pos_relret);assertEquals(2, precComputer.neg_relret);
+		assertEquals(4, precComputer.pos_Rel);assertEquals(3, precComputer.neg_Rel);
+		assertEquals(5, precComputer.pos_Ret);assertEquals(2, precComputer.neg_Ret);
+	}
+
+	@Test
+	public final void test_4()
+	{
+		FSMStructure mach = WMethod.getGraphData(TestFSMAlgo.buildGraph("AM-a->BM-a->AM\nBM-b->CM-a->DM","tmp graph"));
+		PTATestSequenceEngine engine = new PTA_FSMStructure(mach);
+		sequenceSet partialPTA = engine.new sequenceSet();partialPTA.setIdentity();
+		partialPTA = partialPTA.cross(TestFSMAlgo.buildSet(new String[][] {
+				new String[] {"a","a","a","a","b"}, new String[]{"b"}, new String[]{"a", "b", "c"}, new String[]{"a", "b", "a"}
+			}));
+		PTA_computePrecisionRecall precComputer = new PTA_computePrecisionRecall(fsm);
+		precComputer.crossWith(engine);
+		assertEquals(5, precComputer.pos_relret);assertEquals(2, precComputer.neg_relret);
+		assertEquals(6, precComputer.pos_Rel);assertEquals(3, precComputer.neg_Rel);
+		assertEquals(6, precComputer.pos_Ret);assertEquals(3, precComputer.neg_Ret);
+	}
+
+	@Test
+	public final void test_5() // long test sequence (which exists) which is rejected part-way 
+	{
+		FSMStructure mach = WMethod.getGraphData(TestFSMAlgo.buildGraph("AM-a->AM-b->AM-c->AM","tmp graph"));
+		PTATestSequenceEngine engine = new PTA_FSMStructure(mach);
+		sequenceSet partialPTA = engine.new sequenceSet();partialPTA.setIdentity();
+		partialPTA = partialPTA.cross(TestFSMAlgo.buildSet(new String[][] {
+				new String[] {"a","b","b","c","c"}, 
+			}));
+		PTA_computePrecisionRecall precComputer = new PTA_computePrecisionRecall(fsm);
+		precComputer.crossWith(engine);
+		assertEquals(2, precComputer.pos_relret);assertEquals(0, precComputer.neg_relret);
+		assertEquals(5, precComputer.pos_Rel);assertEquals(0, precComputer.neg_Rel);
+		assertEquals(2, precComputer.pos_Ret);assertEquals(3, precComputer.neg_Ret);
+	}
+
+	@Test
+	public final void test_6() // long test sequence (which does not exist) which is rejected part-way 
+	{
+		FSMStructure mach = WMethod.getGraphData(TestFSMAlgo.buildGraph("AM-a->AM-b->AM-c->AM","tmp graph"));
+		PTATestSequenceEngine engine = new PTA_FSMStructure(mach);
+		sequenceSet partialPTA = engine.new sequenceSet();partialPTA.setIdentity();
+		partialPTA = partialPTA.cross(TestFSMAlgo.buildSet(new String[][] {
+				new String[] {"a","b","b","c","c","d"}, 
+			}));
+		PTA_computePrecisionRecall precComputer = new PTA_computePrecisionRecall(fsm);
+		precComputer.crossWith(engine);
+		assertEquals(2, precComputer.pos_relret);assertEquals(1, precComputer.neg_relret);
+		assertEquals(5, precComputer.pos_Rel);assertEquals(1, precComputer.neg_Rel);
+		assertEquals(2, precComputer.pos_Ret);assertEquals(4, precComputer.neg_Ret);
+	}
+
+	@Test
+	public final void test_ign_1() // long test sequence (which exists) which is rejected part-way, but first element of it is ignored. 
+	{
+		FSMStructure mach = WMethod.getGraphData(TestFSMAlgo.buildGraph("AM-a->AM-b->AM-c->AM","tmp graph"));
+		PTATestSequenceEngine engine = new PTA_FSMStructure(mach);
+
+		sequenceSet partialPTA = engine.new sequenceSet();partialPTA.setIdentity();
+		partialPTA = partialPTA.cross(TestFSMAlgo.buildSet(new String[][] {
+				new String[] {"a"}, 
+			}));
+		PTA_computePrecisionRecall precComputer = new PTA_computePrecisionRecall(fsm);
+		precComputer.crossWith(engine);
+		
+		engine = new PTA_FSMStructure(mach);
+		partialPTA = engine.new sequenceSet();partialPTA.setIdentity();
+		partialPTA = partialPTA.cross(TestFSMAlgo.buildSet(new String[][] {
+				new String[] {"a","b","b","c","c"}, 
+			}));
+		precComputer.crossWith(engine);
+		assertEquals(1, precComputer.pos_relret);assertEquals(0, precComputer.neg_relret);
+		assertEquals(4, precComputer.pos_Rel);assertEquals(0, precComputer.neg_Rel);
+		assertEquals(1, precComputer.pos_Ret);assertEquals(3, precComputer.neg_Ret);
+	}
+
+	@Test
+	public final void test_ign2() // a complex structure, most of which gets ignored.
+	{
+		FSMStructure mach = WMethod.getGraphData(TestFSMAlgo.buildGraph("AM-a->BM-a->AM\nBM-b->CM-a->DM","tmp graph"));
+		PTATestSequenceEngine engine = new PTA_FSMStructure(mach);
+		sequenceSet partialPTA = engine.new sequenceSet();partialPTA.setIdentity();
+		partialPTA = partialPTA.cross(TestFSMAlgo.buildSet(new String[][] {
+				new String[] {"a","a","a","a","b"}, new String[]{"b"}, new String[]{"a", "b", "c"}, new String[]{"a", "b", "a"}
+			}));
+		PTA_computePrecisionRecall precComputer = new PTA_computePrecisionRecall(fsm);
+		precComputer.crossWith(engine);
+		precComputer.crossWith(engine);
+		assertEquals(0, precComputer.pos_relret);assertEquals(0, precComputer.neg_relret);
+		assertEquals(0, precComputer.pos_Rel);assertEquals(0, precComputer.neg_Rel);
+		assertEquals(0, precComputer.pos_Ret);assertEquals(0, precComputer.neg_Ret);
+	}
+
+	@Test
+	public final void test_ign3() // long test sequence (which does not exist) which is rejected part-way 
+	{
+		FSMStructure mach = WMethod.getGraphData(TestFSMAlgo.buildGraph("AM-a->AM-b->AM-c->AM","tmp graph"));
+		PTATestSequenceEngine engine = new PTA_FSMStructure(mach);
+		sequenceSet partialPTA = engine.new sequenceSet();partialPTA.setIdentity();
+		partialPTA = partialPTA.cross(TestFSMAlgo.buildSet(new String[][] {
+				new String[] {"a","b","b","c","c","d"}, 
+			}));
+		PTA_computePrecisionRecall precComputer = new PTA_computePrecisionRecall(fsm);
+		precComputer.crossWith(engine);
+		precComputer.crossWith(engine);
+		assertEquals(0, precComputer.pos_relret);assertEquals(1, precComputer.neg_relret);
+		assertEquals(2, precComputer.pos_Rel);assertEquals(1, precComputer.neg_Rel);
+		assertEquals(0, precComputer.pos_Ret);assertEquals(3, precComputer.neg_Ret);
+	}
+}

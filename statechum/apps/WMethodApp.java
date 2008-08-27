1a627c1e33e7df579b1ae2545ec65363d8f2a806
Neil Walkinshaw <nw91@le.ac.uk>
2008-08-27 10:44:33 +0000

    second argument in WMethodApp is optional - doesn't throw an array out of bounds error if not present
    git-svn-id: svn://svn.code.sf.net/p/statechum/code/XMachineTool/trunk@331 4b8d0e13-f52a-0410-bc0e-8c3b9955e25a

--- statechum/apps/WMethodApp.java
+++ statechum/apps/WMethodApp.java
@@ -15,7 +15,7 @@ public class WMethodApp {
 	public static void main(String[] args){
 		LearnerGraph g = LearnerGraph.loadGraph(args[0], Configuration.getDefaultConfiguration());
 		int k = 0;
-		if(args[1]!=null)
+		if(args.length>1)
 			k = Integer.parseInt(args[1]);
 		System.out.println(g.wmethod.getFullTestSet(k));
 	}

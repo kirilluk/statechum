6380ecd99c00e4b64e7cb773455153605ce01a84
Neil Walkinshaw <nw91@le.ac.uk>
2008-12-14 16:54:22 +0000

    a utility class that will output a csv matrix of scores between the states of a machine (as computed by linear). This can be used with tools such as R to output a heatmap, to visualise and cluster groups of similar states.
    git-svn-id: svn://svn.code.sf.net/p/statechum/code/XMachineTool/trunk@406 4b8d0e13-f52a-0410-bc0e-8c3b9955e25a

---
+++ statechum/analysis/learning/util/ScoreMatrixOutput.java
@@ -0,0 +1,68 @@
+package statechum.analysis.learning.util;
+
+import java.io.File;
+import java.util.*;
+
+import edu.uci.ics.jung.graph.impl.DirectedSparseGraph;
+
+import statechum.Configuration;
+import statechum.StringVertex;
+import statechum.analysis.learning.*;
+import statechum.analysis.learning.rpnicore.LearnerGraph;
+import statechum.analysis.learning.rpnicore.LearnerGraphND;
+import statechum.analysis.learning.rpnicore.TestFSMAlgo;
+
+
+public class ScoreMatrixOutput {
+	
+	public static void main(String[] args){
+		Configuration config = Configuration.getDefaultConfiguration();
+		DirectedSparseGraph dsg = TestFSMAlgo.buildGraph("q0-initialise->q1-connect->q2-login->q3-setfiletype->q4-rename->q6-storefile->q5-setfiletype->q4-storefile->q7-appendfile->q5-setfiletype->q4\nq3-makedir->q8-makedir->q8-logout->q16-disconnect->q17\nq3-changedir->q9-listnames->q10-delete->q10-changedir->q9\nq10-appendfile->q11-logout->q16\nq3-storefile->q11\nq3-listfiles->q13-retrievefile->q13-logout->q16\nq13-changedir->q14-listfiles->q13\nq7-logout->q16\nq6-logout->q16", "specgraph");
+		LearnerGraph gr=new LearnerGraph(dsg,config);
+		Stack<PairScore> pairScores = gr.pairscores.chooseStatePairs(0,10,1,null,LearnerGraphND.ignoreRejectStates);
+		ArrayList vertexList = new ArrayList();
+		vertexList.addAll(dsg.getVertices());
+		writeMatrix(pairScores,vertexList,vertexList);
+	}
+
+
+	public static void writeMatrix(List<PairScore> ps, List<StringVertex> stateLabelsA, List<StringVertex> stateLabelsB){
+		String fileRef = statechum.GlobalConfiguration.getConfiguration().getProperty(statechum.GlobalConfiguration.G_PROPERTIES.TEMP)+File.separator+"scoreMatrixOutput.csv";
+		File output  = new File(fileRef);
+		int[][] matrix = new int[stateLabelsA.size()][stateLabelsB.size()];
+		int max = 0;
+		for (PairScore p : ps) {
+			int indexA = stateLabelsA.indexOf(p.getQ());
+			int indexB = stateLabelsB.indexOf(p.getR());
+			int score = p.getScore();
+			matrix[indexA][indexB] = p.getScore();
+			matrix[indexB][indexA] = p.getScore();
+			
+		}
+		String matrixString = matrixToString(matrix,stateLabelsA, stateLabelsB);
+		OutputUtil.write(matrixString, output);
+	}
+	
+	private static String matrixToString(int[][] matrix, List<StringVertex>stateLabelsA, List<StringVertex>stateLabelsB){
+		String s = ",";
+		for(int i=0;i<stateLabelsB.size();i++){
+			if(i<stateLabelsB.size()-1)
+				s = s+stateLabelsB.get(i)+",";
+			else
+				s = s+stateLabelsB.get(i);
+		}
+		s = s + "\n";
+		for(int i=0;i<stateLabelsA.size();i++){
+			s = s + stateLabelsA.get(i)+",";
+			for(int j = 0;j<stateLabelsB.size();j++){
+				if(j<stateLabelsB.size()-1)
+					s = s.concat(String.valueOf(matrix[i][j]))+",";
+				else
+					s = s.concat(String.valueOf(matrix[i][j]));
+			}
+			s = s.concat("\n");
+		}
+		return s;
+	}
+
+}

e0ce3c6008d59154b335c0ac5d69b9a95ae94930
Neil Walkinshaw <nw91@le.ac.uk>
2008-12-22 21:36:38 +0000

    updated for journal experiments
    git-svn-id: svn://svn.code.sf.net/p/statechum/code/XMachineTool/trunk@414 4b8d0e13-f52a-0410-bc0e-8c3b9955e25a

--- statechum/analysis/learning/experiments/ManualAccuracyTracker.java
+++ statechum/analysis/learning/experiments/ManualAccuracyTracker.java
@@ -18,35 +18,79 @@
 package statechum.analysis.learning.experiments;
 
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
 import statechum.analysis.learning.*;
 import statechum.analysis.learning.rpnicore.*;
 import statechum.analysis.learning.observers.*;
 import statechum.analysis.learning.observers.ProgressDecorator.LearnerEvaluationConfiguration;
 import statechum.apps.QSMTool;
+import statechum.model.testset.PTASequenceEngine;
+import statechum.model.testset.PTASequenceEngine.FilterPredicate;
 
 
 public class ManualAccuracyTracker extends QSMTool {
 	
 	public static void main(String[] args){
-		ManualAccuracyTracker tool = new ManualAccuracyTracker();tool.loadConfig(args[0]);tool.runExperiment();
+		ManualAccuracyTracker tool = new ManualAccuracyTracker();tool.runExperiment();
 	}
 	
 	@Override
 	public void runExperiment()
 	{
-		if(active)
-			learnerInitConfiguration.config.setAskQuestions(true);
-		if(!learnerInitConfiguration.ltlSequences.isEmpty())
+		String AutoName = System.getProperty(statechum.GlobalConfiguration.G_PROPERTIES.VIZ_AUTOFILENAME.name());
+		if (AutoName != null) learnerInitConfiguration.config.setAutoAnswerFileName(AutoName);
+		active = true;
+		learnerInitConfiguration.config.setQuestionPathUnionLimit(1);
+		setSimpleConfiguration(learnerInitConfiguration.config, active, k);
+		//if(learnerInitConfiguration.ltlSequences!=null && !learnerInitConfiguration.ltlSequences.isEmpty())
 			learnerInitConfiguration.config.setUseLTL(true);
-		String target = "B-initialise->C-receiveDown->C-sendHalt->E-receiveDown->C-receiveHalt->D\nA-receiveDown->C-monitorHigherPriorityNodes->A-receiveHalt->D-receiveHalt->" +
-		"D-receiveDown->D-sendAck->F-receiveDown->F-receiveHalt->F\nE-receiveAck->G-announceLeadership->C";
+		learnerInitConfiguration.config.setDebugMode(true);
+		//String target = "B-initialise->C-receiveDown->C-sendHalt->E-receiveDown->C-receiveHalt->D\nA-receiveDown->C-monitorHigherPriorityNodes->A-receiveHalt->D-receiveHalt->" +
+		//"D-receiveDown->D-sendAck->F-receiveDown->F-receiveHalt->F\nE-receiveAck->G-announceLeadership->C";
+		
+		String target = "q0-initialise->q1-connect->q2-login->q3-setfiletype->q4-rename->q6-storefile->q5-setfiletype->q4-storefile->q7-appendfile->q5-setfiletype->q4\nq3-makedir->q8-makedir->q8-logout->q16-disconnect->q17\nq3-changedirectory->q9-listfiles->q10-delete->q10-changedirectory->q9\nq10-appendfile->q11-logout->q16\nq3-storefile->q11\nq3-listfiles->q13-retrievefile->q13-logout->q16\nq13-changedirectory->q14-listfiles->q13\nq7-logout->q16\nq6-logout->q16";
+		
 		LearnerGraph targetMachine = new LearnerGraph(TestFSMAlgo.buildGraph(target, "Target"), learnerInitConfiguration.config);
-		RPNILearner l = new RPNIUniversalLearner(null, new LearnerEvaluationConfiguration(null,null,learnerInitConfiguration.config,learnerInitConfiguration.ltlSequences,null));
-		AccuracyTrackerDecorator atd = new AccuracyTrackerDecorator(l,targetMachine);
-		atd.init(sPlus, sMinus);
-		LearnerGraph learned = atd.learnMachine();
-		statechum.analysis.learning.util.OutputUtil.generateDotOutput(learned.pathroutines.getGraph());
-		System.out.println(atd.getResult());
+		
+		
+		int sampleSize = (targetMachine.countEdges()*2);
+		int percentPerChunk = 10;
+		int nrPerChunk = sampleSize/(100/percentPerChunk);nrPerChunk+=nrPerChunk % 2;// make the number even
+		
+		
+		RandomPathGenerator rpg = new RandomPathGenerator(targetMachine, new Random(100),5);// the seed for Random should be the same for each file
+		rpg.generatePosNeg(2*nrPerChunk , 100/percentPerChunk);
+		for(int i=2;i<10;i++){
+			final PTASequenceEngine samples = rpg.getAllSequences(i);
+			
+			PTASequenceEngine.FilterPredicate posPredicate = samples.getFSM_filterPredicate();
+			PTASequenceEngine.FilterPredicate negPredicate = new FilterPredicate() {
+				FilterPredicate origFilter = samples.getFSM_filterPredicate();
+				public boolean shouldBeReturned(Object name) {
+					return !origFilter.shouldBeReturned(name);
+				}
+			};
+			
+			sPlus = new HashSet<List<String>>();
+			sMinus = new HashSet<List<String>>();
+			sPlus.addAll(samples.getData(posPredicate));
+			sMinus.addAll(samples.getData(negPredicate));
+			RPNILearner l = new RPNIUniversalLearner(null, new LearnerEvaluationConfiguration(null,null,learnerInitConfiguration.config,learnerInitConfiguration.ltlSequences,null));
+			//AccuracyTrackerDecorator atd = new  AccuracyTrackerDecorator(new MachineOracleDecorator(l,targetMachine),targetMachine);
+			AccuracyTrackerDecorator atd = new  AccuracyTrackerDecorator(l,targetMachine);
+			Learner autoAns = new AutoAnswers(atd);
+			autoAns.init(sPlus, sMinus);
+			
+			LearnerGraph learned = autoAns.learnMachine();
+			//statechum.analysis.learning.util.OutputUtil.generateDotOutput(learned.pathroutines.getGraph());
+			System.out.println(atd.getResult());
+			
+			System.out.println("------------");
+		}
 	}
 	
 	

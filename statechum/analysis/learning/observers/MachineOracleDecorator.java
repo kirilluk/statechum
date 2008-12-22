9d71e95e3c62e9c89109d55b8dec6ad8eee81241
Neil Walkinshaw <nw91@le.ac.uk>
2008-12-22 21:42:02 +0000

    Decorator to automatically answer questions when supplied with a target machine
    git-svn-id: svn://svn.code.sf.net/p/statechum/code/XMachineTool/trunk@416 4b8d0e13-f52a-0410-bc0e-8c3b9955e25a

---
+++ statechum/analysis/learning/observers/MachineOracleDecorator.java
@@ -0,0 +1,100 @@
+/* Copyright (c) 2006, 2007, 2008 Neil Walkinshaw and Kirill Bogdanov
+ * 
+ * This file is part of StateChum
+ * 
+ * StateChum is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * StateChum is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with StateChum.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
+package statechum.analysis.learning.observers;
+
+/**
+ * This decorator lets you supply the target model so that questions are answered automatically
+ */
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Stack;
+
+import statechum.JUConstants;
+import statechum.Pair;
+import statechum.DeterministicDirectedSparseGraph.CmpVertex;
+import statechum.analysis.learning.AbstractOracle;
+import statechum.analysis.learning.PairScore;
+import statechum.analysis.learning.StatePair;
+import statechum.analysis.learning.oracles.RPNIBlueFringeSootLearner;
+import statechum.analysis.learning.rpnicore.LearnerGraph;
+import statechum.model.testset.PTASequenceEngine;
+
+public class MachineOracleDecorator extends LearnerDecorator {
+
+	LearnerGraph target = null;
+	int restarts=0;int questions=0;
+	
+	public MachineOracleDecorator(Learner learner, LearnerGraph target) {
+		super(learner);
+		this.target=target;
+	}
+
+	public LearnerGraph AddConstraints(LearnerGraph graph) {
+		return decoratedLearner.AddConstraints(graph);
+	}
+
+	public void AugmentPTA(LearnerGraph pta, RestartLearningEnum ptaKind,
+			List<String> sequence, boolean accepted, JUConstants newColour) {
+			decoratedLearner.AugmentPTA(pta, ptaKind, sequence, accepted, newColour);
+	}
+
+	public Pair<Integer, String> CheckWithEndUser(LearnerGraph graph,
+			List<String> question, int expectedAccept, int lengthInHardFacts,
+			Object[] options) {
+		questions++;
+		int answer = target.paths.tracePath(question);
+		return new Pair(answer,null);
+	}
+
+	public Stack<PairScore> ChooseStatePairs(LearnerGraph graph) {
+		return decoratedLearner.ChooseStatePairs(graph);
+	}
+
+	public List<List<String>> ComputeQuestions(PairScore pair,
+			LearnerGraph original, LearnerGraph temp) {
+		return decoratedLearner.ComputeQuestions(pair, original, temp);
+	}
+
+	public LearnerGraph MergeAndDeterminize(LearnerGraph original,
+			StatePair pair) {
+		return decoratedLearner.MergeAndDeterminize(original, pair);
+	}
+
+	public void Restart(RestartLearningEnum mode) {
+		restarts++;
+		decoratedLearner.Restart(mode);
+
+	}
+
+	public String getResult() {
+		return decoratedLearner.getResult()+","+questions+","+restarts;
+	}
+
+	public LearnerGraph init(Collection<List<String>> plus,
+			Collection<List<String>> minus) {
+		return decoratedLearner.init(plus,minus);
+	}
+
+	public LearnerGraph init(PTASequenceEngine engine, int plusSize,
+			int minusSize) {
+		return decoratedLearner.init(engine,plusSize,minusSize);
+	}
+
+}

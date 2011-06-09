257e61dd093bb5c48f0f3e964b3cd6508852ad6f
Ramsay Taylor <r.taylor@dcs.shef.ac.uk>
2011-06-09 18:39:40 +0000

    Fixed stupid typos in export_wrapper.erl
    git-svn-id: svn://svn.code.sf.net/p/statechum/code/XMachineTool/trunk@620 4b8d0e13-f52a-0410-bc0e-8c3b9955e25a

--- ErlangOracle/export_wrapper.erl
+++ ErlangOracle/export_wrapper.erl
@@ -1,13 +1,13 @@
 -module(export_wrapper).
 -export([exec_call_trace/3]).
 
-exec_call_trace(Module, [], OpProc) ->
+exec_call_trace(_Module, [], _OpProc) ->
     ok;
-exec_call_trace(Module, [{Funcion, Args} | Trace], OpProc) ->
+exec_call_trace(Module, [{Function, Args} | Trace], OpProc) ->
     OP = apply(Module, Function, Args),
     OpProc ! {self(), output, {Function, Args, OP}},
     exec_call_trace(Module, Trace, OpProc);
-exec_call_trace(Module, [{Funcion, Args, OP} | Trace], OpProc) ->
+exec_call_trace(Module, [{Function, Args, OP} | Trace], OpProc) ->
     ThisOP = apply(Module, Function, Args),
     if (ThisOP =/= OP) ->
 	    OpProc ! {self(), output_mismatch, {Function, Args, ThisOP}},

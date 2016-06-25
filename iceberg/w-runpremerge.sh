e59ed6aa1da0955317d3fe9c7794bcdefa451f75
Kirill Bogdanov <K.Bogdanov@sheffield.ac.uk>
2016-06-25 18:27:38 +0100

    Reduced the amount of memory given to Java to match the amount I request from Iceberg.

--- iceberg/w-runpremerge.sh
+++ iceberg/w-runpremerge.sh
@@ -6,12 +6,12 @@ EXPERIMENT=statechum.analysis.learning.experiments.MarkovEDSM.MarkovPreMerge
 if [ -z ${SGE_TASK_ID+x} ] || [ "${SGE_TASK_ID}" == "undefined" ];then
 	if [ -z ${STATECHUM_COUNT+x} ];then
 		# thanks to http://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
-		sh ./runexperiment.sh -Xmx13000m ${EXPERIMENT} COLLECT_RESULTS
+		sh ./runexperiment.sh -Xmx12000m ${EXPERIMENT} COLLECT_RESULTS
 	else
-		sh ./runexperiment.sh -Xmx13000m ${EXPERIMENT} COUNT_TASKS 100
+		sh ./runexperiment.sh -Xmx3000m ${EXPERIMENT} COUNT_TASKS 100
 	fi
 else
 # if task id is not "undefined", it means we are running an array task
-	sh ./runexperiment.sh -Xmx13000m ${EXPERIMENT} RUN_TASK $SGE_TASK_ID
+	sh ./runexperiment.sh -Xmx12000m ${EXPERIMENT} RUN_TASK $SGE_TASK_ID
 fi
 

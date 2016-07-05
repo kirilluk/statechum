1a74586a88110fc868d65e8a388f7ae90cd1c50f
Kirill Bogdanov <K.Bogdanov@sheffield.ac.uk>
2016-07-05 05:26:29 +0100

    Reduced requested memory, otherwise jobs do not seem to start for a very long time.

--- iceberg/w-runlearnerevaluation.sh
+++ iceberg/w-runlearnerevaluation.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-#$ -l mem=20G -l rmem=15g -l 'arch=intel*'
+#$ -l mem=16G -l rmem=12g -l 'arch=intel*'
 
 EXPERIMENT=statechum.analysis.learning.experiments.EvaluationOfLearners.EvaluationOfLearners
 
 if [ -z ${SGE_TASK_ID+x} ] || [ "${SGE_TASK_ID}" == "undefined" ];then
 	if [ -z ${STATECHUM_COUNT+x} ];then
 		# thanks to http://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
-		sh ./runexperiment.sh -Xmx13000m ${EXPERIMENT} COLLECT_RESULTS
+		sh ./runexperiment.sh -Xmx11000m ${EXPERIMENT} COLLECT_RESULTS
 	else
 		sh ./runexperiment.sh -Xmx3000m ${EXPERIMENT} COUNT_TASKS 100
 	fi
 else
 # if task id is not "undefined", it means we are running an array task
-	sh ./runexperiment.sh -Xmx13000m ${EXPERIMENT} RUN_TASK $SGE_TASK_ID
+	sh ./runexperiment.sh -Xmx11000m ${EXPERIMENT} RUN_TASK $SGE_TASK_ID
 fi
 
